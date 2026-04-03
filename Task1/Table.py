dd805c6a7924fe0dac610559c8764e81920c260d
Carbon0066 <liyujie1120@outlook.com>
2026-04-03 20:28:14 +0800

    Delete Task1/Table.py

--- Task1/Table.py
+++
@@ -1,8 +0,0 @@
-class Table:
-    def __init__(self, table_id, capacity):
-        self.table_id = table_id
-        self.capacity = capacity
-        self.is_available = True
-
-    def reserve(self):
-        pass
